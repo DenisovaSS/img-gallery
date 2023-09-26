9c2d3816887d5c0ff15d8372bae12890297f7b8b
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-09-26 14:28:11 +0300

    feat: add lisner for button and some style

--- image-galery/java.js
+++ image-galery/java.js
@@ -3,7 +3,8 @@ const pictires = document.querySelectorAll("img");
 const boxImg = document.querySelector(".boxImg");
 const form = document.querySelector("form");
 const input = document.getElementById("input");
-let zapros = "red";
+const mGlass = document.querySelector(".fa-magnifying-glass");
+let zapros = "dogs";
 async function getData(query) {
   const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=landscape&client_id=${key}`;
   const res = await fetch(url);
@@ -12,22 +13,29 @@ async function getData(query) {
   // console.log(data);
 }
 getData(zapros);
+input.focus();
 //full/raw/regular/small/small_s3/thumb
 function picturesCreate(data) {
   boxImg.innerHTML = "";
   data.results.forEach((onePic, index) => {
     const img = `<div class="cover">
   <img src="${onePic.urls.regular}" alt=${index + 1} class="picture" />
-</div>; `;
+</div> `;
     boxImg.insertAdjacentHTML("beforeend", img);
   });
 }
+/////Event listener for  form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  doResearch();
+});
+/////Event listener for button search
+mGlass.addEventListener("click", doResearch);
+function doResearch() {
   if (input.value) {
     let reachZapros = input.value;
     getData(reachZapros);
   } else {
     alert("try again");
   }
-});
+}
